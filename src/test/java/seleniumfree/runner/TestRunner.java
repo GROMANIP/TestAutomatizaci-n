afbc6c39c0cae496912413388b052749b1cdba3c
German Romani <romani.consulting0001@gmail.com>
2024-11-12 22:37:17 -0500

    cambiando ruta de feature

--- src/test/java/seleniumfree/runner/TestRunner.java
+++ src/test/java/seleniumfree/runner/TestRunner.java
@@ -2,16 +2,16 @@ package seleniumfree.runner;
 
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import seleniumfree.pages.BasePage;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src\\test\\resources" }, glue = { "seleniumfree.steps" }, plugin = { "pretty",
+@CucumberOptions(features = { "src\\test\\resources\\feature" }, glue = { "seleniumfree.steps" }, plugin = { "pretty",
         "html:target/cucumber-reports" }, tags = "@Cart")
 
 public class TestRunner{
-
     @AfterClass
     public static void cerrarGhrome() {
     BasePage.cerrarNavegador();
