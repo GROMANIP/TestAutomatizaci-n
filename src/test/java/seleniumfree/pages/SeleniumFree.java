0e7cfaca9b1aa1c257270d97af67c31c5b8fa9e1
German Romani <romani.consulting0001@gmail.com>
2024-12-17 00:05:09 -0500

    se debugeo y se soluciono problemas

--- src/test/java/seleniumfree/pages/SeleniumFree.java
+++ src/test/java/seleniumfree/pages/SeleniumFree.java
@@ -7,6 +7,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SeleniumFree {
 
     private WebDriver driver;
