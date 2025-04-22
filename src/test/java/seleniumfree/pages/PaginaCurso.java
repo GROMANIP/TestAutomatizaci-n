01f23b17034bb125d28947c3eeaa26d5d143777d
German Romani <romani.consulting0001@gmail.com>
2025-04-21 21:47:35 -0500

    Se modifica class PaginaCurso

--- src/test/java/seleniumfree/pages/PaginaCurso.java
+++ src/test/java/seleniumfree/pages/PaginaCurso.java
@@ -3,7 +3,7 @@ package seleniumfree.pages;
 
 public class PaginaCurso extends BasePage {
 
-    private String fundamentoClick = "//a[normalize-space()='Fundamentos del Testing' and @href]";
+    private String fundamentoClick = "//h3[normalize-space()='Introducción al Testing de Software']";
 
     public PaginaCurso() {
         super(driver);
